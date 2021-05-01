aff14c89c1da4b4e04d530447eecd4cbfc8f2955
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-05-01 17:02:09 -0500

    find dog breed by id e2e test

---
+++ src/e2e/specs/find-dogbree.spec.js
@@ -0,0 +1,44 @@
+const axios = require('axios');
+const { expect } = require('chai');
+const { BASE_URL} = require('../../utils/const')
+
+let response;
+describe("When user wants find a dog breed by id", ()=>{
+    before(async()=>{
+        response=  await axios.get(BASE_URL+"/1")
+    });
+
+    it("Then it should return OK 200 status", () =>{
+        expect(response.status).eql(200)
+    
+    });
+
+    it("Then it must return that an element of the list contains all the attributes", () =>{
+        const dogbreed_test  = response.data
+        expect(dogbreed_test).to.have.property("name");
+        expect(dogbreed_test).to.have.property("height");
+        expect(dogbreed_test).to.have.property("weight");
+        expect(dogbreed_test).to.have.property("lifeExpectancy");
+        expect(dogbreed_test).to.have.property("country");
+        expect(dogbreed_test).to.have.property("dogBreedColors");
+        expect(dogbreed_test).to.have.property("dogBreedNatures");
+    });
+
+    it("Then it should return a JSON", () =>{
+        expect(response.headers["content-type"]).to.contain("application/json")
+    });
+
+    it("Then it must return that an element of the list contains all the attributes and each attribute has the corresponding type", () =>{
+        const dogbreed_test  = response.data
+        expect(dogbreed_test.name).to.be.a('string');
+        expect(dogbreed_test.country).to.be.a('string');
+        expect(dogbreed_test.weight).to.be.a('number');
+        expect(dogbreed_test.height).to.be.a('number');
+        expect(dogbreed_test.lifeExpectancy).to.be.a('number');
+        expect(dogbreed_test.dogBreedNatures).to.be.a('array');
+        expect(dogbreed_test.dogBreedColors).to.be.a('array');
+    });
+
+  
+
+});
